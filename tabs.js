24c5480a7a2c66408924ad8c2e06f996e683d0a1
mg@mg.mg <mg@mg.mg>
2020-02-16 16:31:17 +0100

    Tabs, image, backgrounds

---
+++ tabs.js
@@ -0,0 +1,4 @@
+function tabs() {
+    document.getElementById("tabs").innerHTML = '<a href="index.html">O mnie</a> | <a href="cellreset.html">cellRESET</a> | <a href="powercocktail-mix.html">PowerCocktail® Mix Revolution</a> | <a href="proshape-challenge.html">ProShape Challenge®</a> | <a href="sport.html">Sport</a>'
+}
+
