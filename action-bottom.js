141b7b7cf34ca74ecdaed620ce9c236d298b5f4d
mg <mg@mg.mg>
2020-05-02 20:58:33 +0200

    JS for generating tabs and bottom action buttons

---
+++ action-bottom.js
@@ -0,0 +1,28 @@
+function actionBottomContact() {
+    document.getElementById("act-group-contact").innerHTML = "".concat(
+'<a href="tel:+48 666 529 303" class="act-name"><span class="before-act">tel:</span>+48 666 529 303</a>',
+'<a href="mailto:joannan67@gmail.com" class="act-name"><span class="before-act">e-mail:</span>joannan67@gmail.com</a>',
+'<a href="https://www.facebook.com/joana.goyana.3" class="act-name"><span class="before-act">fb:</span>Joanna Nowak</a>');
+}
+
+function actionBottomPm() {
+    document.getElementById("act-group-pm").innerHTML = "".concat(
+'<a href="http://6438073.go4pm.com" class="act-name"><span class="before-act">info:</span>PM-International</a>',
+'<a href="http://6438073.pm-quickstart.com" class="act-name"><span class="before-act"><img src="handshake.svg" class="icon-svg" alt="współpraca:" height="40" width=""></span>PM-International</a>',
+'<a href="http://6438073.fitline.com" class="act-name"><span class="before-act"><img src="shop-trolley.svg" class="icon-svg" alt="sklep:" height="40"></span>FitLine</a>');
+}
+
+function actionBottomMonojony() {
+    document.getElementById("act-group-monojony").innerHTML = "".concat(
+'<a href="https://noveaidea.com/moje-biuro.html" class="act-name"><span class="before-act">sklep:</span>Monojony</a>');
+}
+
+function actionBottomPage() {
+    document.getElementById("act-group-page").innerHTML = "".concat(
+'<a href="https://www.facebook.com/CellReset-z-Asią-i-Madlen-975139695892658/" class="act-name"><span class="before-act"><img src="www.svg" class="icon-svg" alt="sklep:" height="40"></span>Fitline cellReset z Asią i Madlen</a>',
+'<a href="https://www.facebook.com/dobraformazasia/" class="act-name"><span class="before-act"><img src="www.svg" class="icon-svg" alt="sklep:" height="40"></span>Dobra forma z Asią</a>',
+'<a href="https://pl-pl.facebook.com/go2cellreset" class="act-name"><span class="before-act"><img src="www.svg" class="icon-svg" alt="sklep:" height="40"></span>cellRESET Łódź</a>',
+'<a href="https://www.facebook.com/cellRESETOWNIA/" class="act-name"><span class="before-act"><img src="www.svg" class="icon-svg" alt="sklep:" height="40"></span>Moja Przemiana</a>',
+'<a href="https://www.facebook.com/monojonowe/" class="act-name"><span class="before-act"><img src="www.svg" class="icon-svg" alt="sklep:" height="40"></span>Monojony dla zdrowia i urody</a>');
+}
+
--- tabs.js
+++ tabs.js
@@ -1,7 +1,10 @@
 function tabs() {
-    document.getElementById("tabs").innerHTML = '<a href="index.html" class="tab-name">O mnie</a> \
-       <a href="cellreset.html" class="tab-name">cellRESET®</a> \
-       <a href="powercocktail-mix.html" class="tab-name">PowerCocktail® Mix Revolution</a> \
-       <a href="proshape-challenge.html" class="tab-name">ProShape® Challenge</a>  <a href="sport.html" class="tab-name">Sport</a>'
+    document.getElementById("tabs").innerHTML = "".concat(
+        '<a href="index.html" class="tab-name">O mnie</a>',
+        '<a href="cellreset.html" class="tab-name">cellRESET®</a>',
+        '<a href="powercocktail-mix.html" class="tab-name">PowerCocktail® Mix Revolution</a>',
+        '<a href="proshape-challenge.html" class="tab-name">ProShape® Challenge</a>',
+        '<a href="monojony.html" class="tab-name">Monojony</a>',
+        '<a href="sport.html" class="tab-name">Sport</a>');
 }
 
